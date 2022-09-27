7d93c51fb3716724203e0fccfa9cbbc5bbc334ed
2T-T2 <70562172+2T-T2@users.noreply.github.com>
2022-09-28 01:15:00 +0900

    Delete .sample/src/t_panda/sample directory

--- .sample/src/t_panda/sample/event/ScoreChangeListener.java
+++
@@ -1,6 +0,0 @@
-package t_panda.sample.event;
-
-public interface ScoreChangeListener {
-    void add(int p);
-    void sub(int p);
-}
--- .sample/src/t_panda/sample/event/SelectionListener.java
+++
@@ -1,9 +0,0 @@
-package t_panda.sample.event;
-
-import t_panda.game.IGObject;
-import t_panda.sample.Defined.*;
-
-public interface SelectionListener {
-    void pressOkKey(IGObject<Scenes, Tag, Key> obj);
-    void pressCancelKey(IGObject<Scenes, Tag, Key> obj);
-}
